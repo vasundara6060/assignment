a80d03882da919e68c8812d262427fdd1a97e62b
vasundara6060 <144678106+vasundara6060@users.noreply.github.com>
2023-09-12 12:11:00 +0530

    Add files via upload

---
+++ 11.09/task1.py
@@ -0,0 +1,12 @@
+Name=("Vasundara selvam")
+Address=("rasipuram namakkal -637408")
+Class=("Msc computer science")
+T=80
+E=80
+M=90
+Phy=90
+Che=80
+Cs=95
+Total=T+E+M+Phy+Che+Cs
+Avg=Total/6
+print("I'm",Name,"My address",Address,"i'm doing",Class,"My 12th mark:",T,E,M,Phy,Che,Cs,"My total mark",Total,"Avg",Avg)
